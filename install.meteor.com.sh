1915eb3fed0629a160de112564b39d4b70faed5a
Till Schmidt <happysokki@gmail.com>
2018-02-13 14:50:21 +0100

    1.6.1

--- install.meteor.com.sh
+++ install.meteor.com.sh
@@ -30,7 +30,7 @@ run_it () {
 # ~/.meteor, replacing whatever is already there. (~/.meteor is only a cache of
 # packages and package metadata; no personal persistent data is stored there.)
 
-RELEASE="1.6"
+RELEASE="1.6.1"
 
 
 # Now, on to the actual installer!
